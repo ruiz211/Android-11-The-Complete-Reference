0642faef26fd5dcb646e31d9417f299046455c12
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 21:29:29 -0500

    Hello world

--- Projects/1/helloworld/app/src/main/java/com/ruiz211/helloworld/MainActivity.kt
+++ Projects/1/helloworld/app/src/main/java/com/ruiz211/helloworld/MainActivity.kt
@@ -3,35 +3,54 @@ package com.ruiz211.helloworld
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ruiz211.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorldTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            HelloWorld()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!!")
+fun HelloWorld() {
+    Card {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.oak),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                contentScale = ContentScale.Fit
+            )
+            Text(text = "Hello world", style = MaterialTheme.typography.h3)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
-    HelloWorldTheme {
-        Greeting("World")
-    }
+    HelloWorld()
 }
