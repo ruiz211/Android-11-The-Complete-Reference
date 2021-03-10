0f875296ec78cb8f24a49425258d6f7d8951b8c9
ruiz211 <ruiz.im211@gmail.com>
2021-03-10 14:33:42 -0500

    Bottom Navigation View

---
+++ Projects/2/bottomnavigation/app/src/main/java/com/ruiz211/bottomnavigation/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,38 @@
+package com.ruiz211.bottomnavigation.ui.dashboard;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ruiz211.bottomnavigation.R;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel mViewModel;
+
+    public static DashboardFragment newInstance() {
+        return new DashboardFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Projects/2/bottomnavigation/app/src/main/java/com/ruiz211/bottomnavigation/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,7 @@
+package com.ruiz211.bottomnavigation.ui.notifications;
+
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
